598f0892c1653ff966df66515113220333486c89
AlexisRoe <A.Roehrling@gmx.de>
2020-10-06 20:40:00 +0200

    Change list-menu to side-menu

---
+++ src/components/side-menu/side-menu.stories.js
@@ -0,0 +1,10 @@
+import "./side-menu.css";
+import listItemMenu from "./list_item-test.html";
+import listItems from "./list-item.html";
+import sideStats from "./side-menu-stats.html";
+
+export default { title: "Components/Sidebar" };
+
+export const menuTest = () => listItemMenu;
+export const listItem = () => listItems;
+export const stats = () => sideStats;

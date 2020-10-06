5165e6658eb8a1050967563622084a9dbfb75372
AlexisRoe <A.Roehrling@gmx.de>
2020-10-06 14:09:45 +0200

    Change button stories.js

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,8 +1,10 @@
 import "./button.css";
 import buttonPlay from "./button-play.html";
 import buttonShare from "./button-share.html";
+import buttonFavorite from "./button-favorite.html";
 
 export default { title: "Components/Button" };
 
 export const play = () => buttonPlay;
 export const share = () => buttonShare;
+export const favorite = () => buttonFavorite;

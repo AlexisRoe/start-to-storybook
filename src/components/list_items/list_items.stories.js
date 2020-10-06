8b2d21e182038c9f3f3110812065129940fcea68
AlexisRoe <A.Roehrling@gmx.de>
2020-10-06 15:42:41 +0200

    Add menu story

--- src/components/list_items/list_items.stories.js
+++ src/components/list_items/list_items.stories.js
@@ -1,6 +1,8 @@
 import "./list_items.css";
 import listItemMenu from "./list_item_menu.html";
+import listMenu from "./list_menu.html";
 
-export default { title: "Components/List-Items" };
+export default { title: "Components/Menu" };
 
 export const listItem = () => listItemMenu;
+export const Menu = () => listMenu;

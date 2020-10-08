54be01f28f167c70fa11e49ef64a9e134ecfc944
AlexisRoe <A.Roehrling@gmx.de>
2020-10-08 17:57:35 +0200

    Complete now playing nav

--- src/components/nav-head/nav-head.stories.js
+++ src/components/nav-head/nav-head.stories.js
@@ -2,9 +2,11 @@ import "./nav-head.css";
 import navheads from "./nav-head.html";
 import navheadStandards from "./nav-head-standard.html";
 import navheadStandardsBack from "./nav-head-standard-back.html";
+import navheadNowPlayings from "./nav-head-now-playing.html";
 
 export default { title: "Components/Navigation on Top" };
 
 export const navhead = () => navheads;
 export const navheadStandard = () => navheadStandards;
 export const navheadStandardBack = () => navheadStandardsBack;
+export const navheadNowPlaying = () => navheadNowPlayings;

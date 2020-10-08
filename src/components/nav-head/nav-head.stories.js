56266758ff756a7fbafbc7eb99f3086139abc612
AlexisRoe <A.Roehrling@gmx.de>
2020-10-08 17:52:01 +0200

    Add standard back site

--- src/components/nav-head/nav-head.stories.js
+++ src/components/nav-head/nav-head.stories.js
@@ -1,8 +1,10 @@
 import "./nav-head.css";
 import navheads from "./nav-head.html";
 import navheadStandards from "./nav-head-standard.html";
+import navheadStandardsBack from "./nav-head-standard-back.html";
 
 export default { title: "Components/Navigation on Top" };
 
 export const navhead = () => navheads;
 export const navheadStandard = () => navheadStandards;
+export const navheadStandardBack = () => navheadStandardsBack;

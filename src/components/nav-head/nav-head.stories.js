b1d25b0ce6f81c3690673c87bb00a0bd9915fabb
AlexisRoe <A.Roehrling@gmx.de>
2020-10-08 17:47:44 +0200

    Add standard header nav

--- src/components/nav-head/nav-head.stories.js
+++ src/components/nav-head/nav-head.stories.js
@@ -1,6 +1,8 @@
 import "./nav-head.css";
 import navheads from "./nav-head.html";
+import navheadStandards from "./nav-head-standard.html";
 
 export default { title: "Components/Navigation on Top" };
 
 export const navhead = () => navheads;
+export const navheadStandard = () => navheadStandards;

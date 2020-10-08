ec2aa561c9e345d8de705033f4f1d19a8d40b80b
AlexisRoe <A.Roehrling@gmx.de>
2020-10-08 17:59:46 +0200

    Add driver mode head

--- src/components/nav-head/nav-head.stories.js
+++ src/components/nav-head/nav-head.stories.js
@@ -3,6 +3,7 @@ import navheads from "./nav-head.html";
 import navheadStandards from "./nav-head-standard.html";
 import navheadStandardsBack from "./nav-head-standard-back.html";
 import navheadNowPlayings from "./nav-head-now-playing.html";
+import navheadDriverModes from "./nav-head-driver-mode.html";
 
 export default { title: "Components/Navigation on Top" };
 
@@ -10,3 +11,4 @@ export const navhead = () => navheads;
 export const navheadStandard = () => navheadStandards;
 export const navheadStandardBack = () => navheadStandardsBack;
 export const navheadNowPlaying = () => navheadNowPlayings;
+export const navheadDriverMode = () => navheadDriverModes;

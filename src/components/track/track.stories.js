e9cbdab1e63d60bb6f095e1fad62f94d2ced31a7
AlexisRoe <A.Roehrling@gmx.de>
2020-10-12 16:58:51 +0200

    Delete not useful statements

--- src/components/track/track.stories.js
+++ src/components/track/track.stories.js
@@ -3,5 +3,3 @@ import {createTrackElement} from "./track.js";
 export default { title: "Components/Track" };
 
 export const beatIt = () => createTrackElement("Michael Jackson", "Billie Jean");
-/* export const yeah = () => createTrackElement("Bebe Rexa", "Be the girl");
-export const yamm = () => createTrackElement("Daughtry", "Countryman"); */

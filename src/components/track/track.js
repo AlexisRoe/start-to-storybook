8a5c6fd5b238eadf00cdfc1ad75554aa68393678
AlexisRoe <A.Roehrling@gmx.de>
2020-10-12 14:40:21 +0200

    Complete track componente as flex container

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,29 +1,40 @@
 import imageDog from "../../assets/dog.jpg"; // import picture for appending
+import playIcon from "../../assets/icon-play-color.svg"; // import picture for appending
 
 export function createTrackElement() {
+    const artist = "Michael Jackson";
+    const title = "Billie Jean";
+
+    /* Container */
     const listItem = document.createElement("li");
 
+    // artist picture
     const imgArtist = document.createElement("img");
     imgArtist.src = imageDog; // for demo purposes
+    imgArtist.alt = `artist - ${artist}`;
     imgArtist.classList.add("artist"); // add class of name artist to image
 
     listItem.appendChild(imgArtist);
 
+    // metadata
     const songContainer = document.createElement("div");
-    const artist = document.createElement("span");
-    artist.innerHTML = "Some Artist"; // for demo purposes
-    const album = document.createElement("span");
-    album.innerHTML = "a album title"; // for demo purposes
-    const playButton = document.createElement("button");
-    playButton.type = "submit"; // type definition
-    playButton.innerHTML = "Play"; // for demo purposes
-
-    songContainer.appendChild(artist);
-    songContainer.appendChild(album);
-    songContainer.appendChild(playButton);
-
+    const albumSpan = document.createElement("span");
+    albumSpan.innerHTML = title; // for demo purposes
+    const artistSpan = document.createElement("span");
+    artistSpan.innerHTML = artist; // for demo purposes
+    
+    songContainer.appendChild(albumSpan, artistSpan);
     listItem.appendChild(songContainer);
 
+    // play-button
+    const playButton = document.createElement("button");
+    playButton.type = "submit"; // type definition
+    const playImage = document.createElement("img");
+    playImage.src = playIcon;
+
+    playButton.appendChild(playImage);
+    listItem.appendChild(playButton);
+    
     return listItem;
 }
 

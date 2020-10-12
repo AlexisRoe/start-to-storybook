158e5f599a7fa1348021220e06072e37ccee970e
AlexisRoe <A.Roehrling@gmx.de>
2020-10-12 16:55:23 +0200

    Change to responsive units

--- src/components/track/track.stories.js
+++ src/components/track/track.stories.js
@@ -3,3 +3,5 @@ import {createTrackElement} from "./track.js";
 export default { title: "Components/Track" };
 
 export const beatIt = () => createTrackElement("Michael Jackson", "Billie Jean");
+/* export const yeah = () => createTrackElement("Bebe Rexa", "Be the girl");
+export const yamm = () => createTrackElement("Daughtry", "Countryman"); */

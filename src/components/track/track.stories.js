f32af5f3f7f630b00c50e1c3c104dc018b24a4d1
AlexisRoe <A.Roehrling@gmx.de>
2020-10-14 17:00:05 +0200

    Add forEach loop for building playlist, modify css

--- src/components/track/track.stories.js
+++ src/components/track/track.stories.js
@@ -23,3 +23,35 @@ export const christmasAdventure2 = () => {
     
     return createTrackElement(track);
 }
+
+/* track array */
+const playlist = [
+{
+    artist: "TRG Banks", 
+    title: "A Christmas Adventure",
+    audioSrc: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/TRG_Banks/TRG_Banks_Christmas_Album/TRG_Banks_-_10_-_A_Christmas_adventure_Part_3.mp3", 
+    trackImageSource: "https://files.freemusicarchive.org/storage-freemusicarchive-org/images/albums/TRG_Banks_-_TRG_Banks_Christmas_Album_-_20180915194354202.png?method=crop&width=290&height=290"
+},
+{
+    artist: "TRG Banks",
+    title: "A Christmas Adventure",
+    trackImageSource: "https://files.freemusicarchive.org/storage-freemusicarchive-org/images/albums/TRG_Banks_-_TRG_Banks_Christmas_Album_-_20180915194354202.png?method=crop&width=290&height=290"
+},
+{
+    artist: "Michael Jackson", 
+    title: "Billie Jean", 
+    trackImageSource: dog
+}
+];
+
+ export const songs = () => {
+    const container = document.createElement("ul");
+    container.className = "container";
+
+    playlist.forEach( track => {
+        const element = createTrackElement(track);
+        container.append(element);
+    });
+
+    return container;
+};

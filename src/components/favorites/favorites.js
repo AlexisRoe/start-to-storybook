ed049487236bf1205ff01ca95e67129f5da1a4df
AlexisRoe <A.Roehrling@gmx.de>
2020-10-19 10:21:41 +0200

    Refactore code, build in array.map()

--- src/components/favorites/favorites.js
+++ src/components/favorites/favorites.js
@@ -10,113 +10,111 @@ import heartYellow from "../../assets/favorite/heart-yellow.svg";
 import heartGreen from "../../assets/favorite/heart-green.svg";
 import trashCan from "../../assets/favorite/delete_forever-24px.svg";
 
-
 function heartCounter(event) {
-    const keyColor = event.target.alt;
-    
-    if (storageAvailable("localStorage")) {
-        if (localStorage.getItem(keyColor)) { // if keyColor exists
-            const oldCount = Number(localStorage.getItem(keyColor)); // get old value in storage
-            const newCount = oldCount + 1; // increase counter
-            localStorage.setItem(keyColor, newCount); 
-            console.log(`value: ${keyColor} found and increased by 1`);
-        } else {
-            localStorage.setItem(keyColor, "1");
-            console.log(`value: ${keyColor} set`);
-        }
+  const keyColor = event.target.alt;
+
+  if (storageAvailable("localStorage")) {
+    if (localStorage.getItem(keyColor)) {
+      const count = Number(localStorage.getItem(keyColor)) + 1;
+      localStorage.setItem(keyColor, count);
+      console.log(`value: ${keyColor} found and increased by 1`);
     } else {
-        console.log(`localStorage disabled`);
+      localStorage.setItem(keyColor, "1");
+      console.log(`value: ${keyColor} set`);
     }
-    scoreboard();
-};
+  } else {
+    console.log(`localStorage disabled`);
+  }
+  scoreboard();
+}
 
 function scoreboard() {
-    let scoreboard = "** SCOREBOARD **\n";
+  let scoreboard = "** SCOREBOARD **\n";
 
-    if (storageAvailable("localStorage")) {
-        if (Object.keys(localStorage).length === 0) {
-            scoreboard = `${scoreboard} no scores available`;
-        } else {
-            Object.getOwnPropertyNames(localStorage).forEach( keyColor => {
-                scoreboard = scoreboard + "\n" + `${keyColor}: ${localStorage[keyColor]}`;
-            });
-        };
+  if (storageAvailable("localStorage")) {
+    if (Object.keys(localStorage).length === 0) {
+      scoreboard = `${scoreboard} no scores available`;
     } else {
-        console.log(`localStorage disabled`);
+      Object.getOwnPropertyNames(localStorage).forEach((keyColor) => {
+        scoreboard =
+          scoreboard + "\n" + `${keyColor}: ${localStorage[keyColor]}`;
+      });
     }
-    document.querySelector("#scoreboard").value = scoreboard;
-};
+  } else {
+    console.log(`localStorage disabled`);
+  }
+  document.querySelector("#scoreboard").value = scoreboard;
+}
 
 function cleanStorage() {
-    localStorage.clear();
-    document.querySelector("#scoreboard").value = "** SCOREBOARD **\n";
-    console.log("Storage clear");
-};
+  localStorage.clear();
+  document.querySelector("#scoreboard").value = "** SCOREBOARD **\n";
+  console.log("Storage clear");
+}
 
+export function createFavorites() {
+  const favoriteImages = [
+    {
+      src: heartOrange,
+      alt: "orange-hearth",
+      onclick: (event) => {
+        heartCounter(event);
+      },
+    },
+    {
+      src: heartBlue,
+      alt: "blue-hearth",
+      onclick: (event) => {
+        heartCounter(event);
+      },
+    },
+    {
+      src: heartPink,
+      alt: "pink-hearth",
+      onclick: (event) => {
+        heartCounter(event);
+      },
+    },
+    {
+      src: heartYellow,
+      alt: "yellow-hearth",
+      onclick: (event) => {
+        heartCounter(event);
+      },
+    },
+    {
+      src: heartGreen,
+      alt: "green-hearth",
+      onclick: (event) => {
+        heartCounter(event);
+      },
+    },
+    {
+      src: trashCan,
+      alt: "Delete everything",
+      onclick: () => {
+        cleanStorage();
+      },
+    },
+  ];
 
-export function createFavorites () {
+  const heartChildrens = favoriteImages.map((favoriteImage) =>
+    createElement("img", favoriteImage)
+  );
 
-    const container = createElement("div", {
-        className: "container",
-        children:
-        [
-            createElement("div",{
-                className: "hearts",
-                children:
-                [
-                    createElement("img", {
-                        src: heartOrange,
-                        alt: "orange-hearth",
-                        onclick: (event) => {
-                            heartCounter(event);
-                        },
-                    }),
-                    createElement("img", {
-                        src: heartBlue,
-                        alt: "blue-hearth",
-                        onclick: (event) => {
-                            heartCounter(event);
-                        },
-                    }),
-                    createElement("img", {
-                        src: heartPink,
-                        alt: "pink-hearth",
-                        onclick: (event) => {
-                            heartCounter(event);
-                        },
-                    }),
-                    createElement("img", {
-                        src: heartYellow,
-                        alt: "yellow-hearth",
-                        onclick: (event) => {
-                            heartCounter(event);
-                        },
-                    }),
-                    createElement("img", {
-                        src: heartGreen,
-                        alt: "green-hearth",
-                        onclick: (event) => {
-                            heartCounter(event);
-                        },
-                    }),
-                    createElement("img", {
-                        id: "trash",
-                        src: trashCan,
-                        alt: "Delete everything!",
-                        onclick: () => {
-                            cleanStorage();
-                        },
+  const container = createElement("div", {
+    className: "container",
+    children: [
+      createElement("div", {
+        className: "hearts",
+        children: heartChildrens,
+      }),
+      createElement("textarea", {
+        className: "output",
+        id: "scoreboard",
+      }),
+    ],
+  });
 
-                    }),
-                ],
-            }),
-            createElement("textarea", {
-                className: "output",
-                id: "scoreboard",
-            }),
-        ],
-    });
-
-    return container;
-};
-
+  return container;
+}
